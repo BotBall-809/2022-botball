cc41b27b9bede0e0fa23540cf6f0377adb41cb7f
SidharthManghnani <101518562+SidharthManghnani@users.noreply.github.com>
2022-05-14 08:52:00 -0400

    Create movement.h

---
+++ Wombat/movement.h
@@ -0,0 +1,9 @@
+
+void forward(int mm, int speed);
+void forwardForSeconds(int speed, int timeMillis);
+
+void turn_left_spin(int degrees);
+void turn_right_spin(int degrees);
+void rings();
+void turn_left_pivot(int degrees);
+void turn_right_pivot(int degrees);
